704985bfc8bcfff4428559820779925061a665e3
jimrodriguez20-art <jimrodriguez299@gmail.com>
2026-09-25 00:09:24 -0500

    Add boolean evaluation and print statement

--- Corte1/Tarea1/tryt.py
+++ Corte1/Tarea1/tryt.py
@@ -1,1 +1,4 @@
-
+# Evalúa la expresión '7 + 8 == 11' (donde '7 + 8' resulta en 15, y '15 == 11' es falso) y asigna el resultado booleano 'False' a 'my_cool_variable'; evalúa y almacena una comparación lógica.
+my_cool_variable = 7 + 8 == 11
+# Imprime en consola el valor almacenado en la variable 'my_cool_variable'; muestra en pantalla el resultado booleano 'False'.
+print(my_cool_variable)
